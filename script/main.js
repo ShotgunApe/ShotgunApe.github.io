c163d4151b3c46aa0bf752526ead684eda3a8cf8
ShotgunApe <shotgunayyype@gmail.com>
2021-06-14 22:34:39 -0700

    last modified

--- script/main.js
+++ script/main.js
@@ -7,6 +7,8 @@ function req(username) {
     xhr.onload = function() {
         const data = JSON.parse(this.response);
         console.log(data);
+
+        document.getElementById("lastModified").innerHTML = "last modified: " + data[1].updated_at.slice(0, 10) + " at " + data[1].updated_at.slice(11, 19) + " utc";
     }
     xhr.send();
 }
