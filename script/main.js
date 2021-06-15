41908d53f947e57a9ccf9a9220e8bbc94acab84c
ShotgunApe <shotgunayyype@gmail.com>
2021-06-15 14:04:36 -0700

    thanks cj

--- script/main.js
+++ script/main.js
@@ -4,11 +4,15 @@ function req(username) {
 
     xhr.open('GET', url, true);
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         const data = JSON.parse(this.response);
         console.log(data);
 
-        document.getElementById("lastModified").innerHTML = "last modified: " + data[1].updated_at.slice(0, 10) + " at " + data[1].updated_at.slice(11, 19) + " utc";
+        for (i = 0; i < data.length; i++) {
+            if (data[i].name = "osutools") {
+                document.getElementById("lastModified").innerHTML = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
+            }
+        }
     }
     xhr.send();
 }
