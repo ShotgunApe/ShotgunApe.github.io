18ca03066a80f46960084dae43f1de9d3e4f0d33
ownsterDev <piccolowen@gmail.com>
2021-06-29 10:09:19 -0400

    fixed formatting and changed some things

--- script/main.js
+++ script/main.js
@@ -1,16 +1,19 @@
+const xhr = new XMLHttpRequest();
+//I hate semicolons
+
 function req(username) {
-    const xhr = new XMLHttpRequest();
     const url = `https://api.github.com/users/${username}/repos`;
 
     xhr.open('GET', url, true);
 
     xhr.onload = function () {
         const data = JSON.parse(this.response);
-        console.log(data);
+        //For debug:
+        //console.log(data);
 
         for (i = 0; i < data.length; i++) {
             if (data[i].name == "ShotgunApe.github.io") {
-                document.getElementById("lastModified").innerHTML = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
+                document.getElementById("lastModified").innerText = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
             }
         }
     }
