9c943628be3fcd5acfdec1b1236cb868dea3af5e
ShotgunApe <shotgunayyype@gmail.com>
2021-12-15 00:50:25 -0500

    adding new page

--- script/main.js
+++ script/main.js
@@ -9,12 +9,27 @@ function req(username) {
     xhr.onload = function () {
         const data = JSON.parse(this.response);
         //For debug:
-        //console.log(data);
+        console.log(data);
 
         for (i = 0; i < data.length; i++) {
             if (data[i].name == "ShotgunApe.github.io") {
                 document.getElementById("lastModified").innerText = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
             }
+        }
+    }
+    xhr.send();
+}
+
+function proj(username) {
+    const url = `https://api.github.com/users/${username}/repos`;
+
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+        const data = JSON.parse(this.reponse);
+        
+        for (i = 0; i < data.length; i++) {
+            document.getElementById("projectsGithub").innerText = data[i].name;
         }
     }
     xhr.send();
