9b56561e3b962c4b681bd16f188fd0c1bd7eee9d
ShotgunApe <shotgunayyype@gmail.com>
2021-12-16 16:14:46 -0500

    Changes to main.js

--- script/main.js
+++ script/main.js
@@ -8,29 +8,27 @@ function req(username) {
 
     xhr.onload = function () {
         const data = JSON.parse(this.response);
-        //For debug:
-        console.log(data);
 
-        for (i = 0; i < data.length; i++) {
-            if (data[i].name == "ShotgunApe.github.io") {
-                document.getElementById("lastModified").innerText = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
-            }
+        last_updated(data)
+
+        if (document.getElementById(`projectsGithub0`) != null) {
+            proj(data)
         }
+        
     }
     xhr.send();
 }
 
-function proj(username) {
-    const url = `https://api.github.com/users/${username}/repos`;
+function proj(data) {
+    for (i = 0; i < data.length; i++) {
+        document.getElementById(`projectsGithub${i}`).innerHTML = "<a href='" + data[i].html_url + "' target='_blank' style='color:teal'>'" + data[i].name + "' </a>";
+    }
+}
 
-    xhr.open('GET', url, true);
-
-    xhr.onload = function () {
-        const data = JSON.parse(this.response);
-        
-        for (i = 0; i < data.length; i++) {
-            document.getElementById(`projectsGithub${i}`).innerText = data[i].name;
+function last_updated(data) {
+    for (i = 0; i < data.length; i++) {
+        if (data[i].name == "ShotgunApe.github.io") {
+            document.getElementById("lastModified").innerText = "Last modified: " + data[i].updated_at.slice(0, 10) + " at " + data[i].updated_at.slice(11, 19) + " utc";
         }
     }
-    xhr.send();
 }
